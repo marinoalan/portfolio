80441aafb76615332f3d6937161198c5f67f973c
Alan Marino <marinoalanunq@gmail.com>
2022-04-19 21:15:34 +0200

    Added blur loading on image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,6 +48,25 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const shimmer = (w: string, h: string) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -64,6 +83,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Image
             src="/profile-img.png"
             alt="Picture of the author"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimmer("40%", "40%")
+            )}`}
             layout="fill"
             objectFit="contain"
             priority
