9dcae946b9939c516e232cf540e3194d2a90f038
Alan Marino <marinoalanunq@gmail.com>
2022-04-26 17:18:02 +0200

    Added more text to portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,19 @@ import styled from "styled-components";
 
 const CenteredText = styled.p`
   text-align: center;
+  margin-block: 0px;
+  margin-top: 2rem;
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const TextContainer = styled.div`
+  max-width: 44rem;
 `;
 
 const millisecondsSecond = 1000;
@@ -20,13 +33,28 @@ const getAge = (dateString: string) => {
 
 const Home: NextPage = () => {
   return (
-    <>
-      <CenteredText>
-        Hi! I'm Alan Marino, a {getAge("1994-08-07")}-year-old Argentinian
-        software developer, graduated at National University of Quilmes and
-        recently based in Italy.
-      </CenteredText>
-    </>
+    <TextWrapper>
+      <TextContainer>
+        <CenteredText>
+          Hi! 👋 I'm Alan Marino, a {getAge("1994-08-07")}-year-old Argentinian
+          software developer, graduated at National University of Quilmes and
+          recently based in Italy.
+          <br />
+          <br />
+          I started my career working as a Fullstack developer using JAVA and
+          Angular for backend and frontend side, respectively.
+          <br />
+          <br />
+          In that experience i realized that i enjoyed coding and building
+          frontend-oriented projects.
+          <br />
+          <br />
+          Consequently i got a new job as a Frontend developer using frameworks,
+          libraries and databases that i never used, it was a great challenge
+          for me to quickly learn Meteor, React, MongoDB, Node, etc.
+        </CenteredText>
+      </TextContainer>
+    </TextWrapper>
   );
 };
 
