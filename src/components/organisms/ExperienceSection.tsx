c3886d96fb97658f4c60bb401d11a638a728e608
Alan Marino <marinoalanunq@gmail.com>
2022-06-05 13:28:28 +0200

    Added responsive view on experience

--- src/components/organisms/ExperienceSection.tsx
+++ src/components/organisms/ExperienceSection.tsx
@@ -14,50 +14,57 @@ const Ul = styled.ul`
   flex-direction: column;
   row-gap: 40px;
   padding: 0;
-  position: relative;
 
-  &:before {
-    content: "";
-    position: absolute;
-    height: 100%;
-    width: 2px;
-    left: calc(50% - 1px);
-    background-color: var(--lineColor);
+  @media (min-width: 56.5rem) {
+    position: relative;
+
+    &:before {
+      content: "";
+      position: absolute;
+      height: 100%;
+      width: 2px;
+      left: calc(50% - 1px);
+      background-color: var(--lineColor);
+    }
   }
 `;
 
 const Li = styled.li`
   background-color: #405164;
-  width: calc(50% - 15px);
-  position: relative;
 
-  &:after {
-    content: "";
-    position: absolute;
-    height: 12px;
-    width: 12px;
-    border-radius: 50%;
-    background-color: var(--fontColor);
-    top: -12px;
-  }
+  border-radius: 10px;
 
-  &:nth-child(even) {
-    margin-left: 50%;
-    transform: translateX(15px);
-    border-radius: 0 10px 10px 10px;
-
+  @media (min-width: 56.5rem) {
+    position: relative;
+    width: calc(50% - 15px);
     &:after {
-      left: 0;
-      transform: translateX(-21px);
+      content: "";
+      position: absolute;
+      height: 12px;
+      width: 12px;
+      border-radius: 50%;
+      background-color: var(--fontColor);
+      top: -12px;
     }
-  }
 
-  &:nth-child(odd) {
-    border-radius: 10px 0 10px 10px;
+    &:nth-child(even) {
+      margin-left: 50%;
+      transform: translateX(15px);
+      border-radius: 0 10px 10px 10px;
 
-    &:after {
-      right: 0;
-      transform: translateX(21px);
+      &:after {
+        left: 0;
+        transform: translateX(-21px);
+      }
+    }
+
+    &:nth-child(odd) {
+      border-radius: 10px 0 10px 10px;
+
+      &:after {
+        right: 0;
+        transform: translateX(21px);
+      }
     }
   }
 `;
@@ -70,6 +77,19 @@ const Article = styled.article`
   padding: 20px;
 `;
 
+const ArticleTitle = styled.h3`
+  margin: 0;
+`;
+
+const ArticleDate = styled.h4`
+  margin: 0;
+
+  @media (min-width: 56.5rem) {
+    top: -24px;
+    position: absolute;
+  }
+`;
+
 const ExperienceSection: FunctionComponent = () => (
   <Section id="experience" title="Experience">
     <Container>
@@ -77,19 +97,22 @@ const ExperienceSection: FunctionComponent = () => (
         <Ul>
           <Li>
             <Article>
-              <h3>Accenture Argentina</h3>
+              <ArticleDate>December 2019 - March 2022</ArticleDate>
+              <ArticleTitle>Accenture Argentina</ArticleTitle>
               <p>Hice tal cosa</p>
             </Article>
           </Li>
           <Li>
             <Article>
-              <h3>Atix Labs</h3>
+              <ArticleDate>May 2019 - December 2019</ArticleDate>
+              <ArticleTitle>Atix Labs</ArticleTitle>
               <p>Hice tal cosa</p>
             </Article>
           </Li>
           <Li>
             <Article>
-              <h3>Softtek Argentina</h3>
+              <ArticleDate>July 2018 - May 2019</ArticleDate>
+              <ArticleTitle>Softtek Argentina</ArticleTitle>
               <p>Hice tal cosa</p>
             </Article>
           </Li>
